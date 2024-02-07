436c83c86922b94be56e2bc32fa0964d737ea775
Klaus <47281955+Promethium147@users.noreply.github.com>
2024-02-07 08:47:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+# You have to install the Webdriver Manager in the console with:
+# pip install webdriver-manager
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
